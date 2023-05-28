1e50627ae5c326451ad54654197d60b18c6328ae
melody <jamc.melody@gmail.com>
2023-05-28 15:11:28 +0800

    feat: using Langchain with FastAPI to create a stream response

---
+++ langchain/fastapi_stream.py
@@ -0,0 +1,56 @@
+"""The Latest Langchain version has made streaming even more user-friendly.
+"""
+import asyncio
+import os
+from typing import AsyncIterable
+
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
+from langchain.callbacks import AsyncIteratorCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+from pydantic import BaseModel
+
+# two ways to load env variables
+# 1.load env variables from .env file
+load_dotenv()
+
+# 2.manually set env variables
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = ""
+
+app = FastAPI()
+
+
+async def send_message(message: str) -> AsyncIterable[str]:
+    callback = AsyncIteratorCallbackHandler()
+    model = ChatOpenAI(
+        streaming=True,
+        verbose=True,
+        callbacks=[callback],
+    )
+
+    # Begin a task that runs in the background.
+    task = asyncio.create_task(model.agenerate(messages=[[HumanMessage(content=message)]]))
+
+    async for token in callback.aiter():
+        # Use server-sent-events to stream the response
+        yield f"data: {token}\n\n"
+
+    await task
+
+
+class StreamRequest(BaseModel):
+    """Request body for streaming."""
+    message: str
+
+
+@app.post("/stream")
+def stream(body: StreamRequest):
+    return StreamingResponse(send_message(body.message), media_type="text/event-stream")
+
+
+if __name__ == "__main__":
+    uvicorn.run(host="0.0.0.0", port=8000, app=app)
---
+++ langchain/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# This script is used to test the langchain.
+curl "http://127.0.0.1:8000/stream" -X POST -d '{"message": "hello!"}' -H 'Content-Type: application/json'
