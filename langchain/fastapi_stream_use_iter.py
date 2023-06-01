334304fbb5af58ed11a77b923e9122e1d95c7a8c
melody <jamc.melody@gmail.com>
2023-06-01 16:27:46 +0800

    fix(langchain): set aiter done when exception

--- langchain/fastapi_stream_use_iter.py
+++ langchain/fastapi_stream_use_iter.py
@@ -4,7 +4,7 @@ making it easier to implement streaming functionality in your applications.
 """
 import asyncio
 import os
-from typing import AsyncIterable
+from typing import AsyncIterable, Awaitable
 
 import uvicorn
 from dotenv import load_dotenv
@@ -15,7 +15,7 @@ from langchain.chat_models import ChatOpenAI
 from langchain.schema import HumanMessage
 from pydantic import BaseModel
 
-# two ways to load env variables
+# Two ways to load env variables
 # 1.load env variables from .env file
 load_dotenv()
 
@@ -34,8 +34,22 @@ async def send_message(message: str) -> AsyncIterable[str]:
         callbacks=[callback],
     )
 
+    async def wrap_done(fn: Awaitable, event: asyncio.Event):
+        """Wrap an awaitable with a event to signal when it's done or an exception is raised."""
+        try:
+            await fn
+        except Exception as e:
+            # TODO: handle exception
+            print(f"Caught exception: {e}")
+        finally:
+            # Signal the aiter to stop.
+            event.set()
+
     # Begin a task that runs in the background.
-    task = asyncio.create_task(model.agenerate(messages=[[HumanMessage(content=message)]]))
+    task = asyncio.create_task(wrap_done(
+        model.agenerate(messages=[[HumanMessage(content=message)]]),
+        callback.done),
+    )
 
     async for token in callback.aiter():
         # Use server-sent-events to stream the response
