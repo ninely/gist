7fac3a17ac7bac60d81b32c59b890ffc89c7f28b
melody <jamc.melody@gmail.com>
2023-06-16 16:57:34 +0800

    feat(langchain): add final aiter callback for agent

--- langchain/test.sh
+++ langchain/test.sh
@@ -2,3 +2,5 @@
 
 # This script is used to test the langchain.
 curl "http://127.0.0.1:8000/stream" -X POST -d '{"message": "hello!"}' -H 'Content-Type: application/json'
+
+curl "http://127.0.0.1:8000/stream/agent" -X POST -H 'Content-Type: application/json'
